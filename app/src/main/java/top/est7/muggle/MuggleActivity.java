1b98986dd026d9ad695aa59e59a3c31129e7a2ba
est7 <youremail>
2016-11-23 01:14:53 +0800

    fisrt commit for test

--- app/src/main/java/top/est7/muggle/MuggleActivity.java
+++ app/src/main/java/top/est7/muggle/MuggleActivity.java
@@ -1,13 +1,19 @@
 package top.est7.muggle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MuggleActivity extends AppCompatActivity {
+
+    private View mTextClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muggle);
+
+        mTextClock = findViewById(R.id.text_clock);
     }
+
 }
