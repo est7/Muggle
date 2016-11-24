2af830cec6829eae58a05961b6c267e6389d3bf4
est7 <youremail>
2016-11-25 02:14:28 +0800

    整理了一下引入第三方库的版本

--- app/src/main/java/top/est7/muggle/MuggleActivity.java
+++ app/src/main/java/top/est7/muggle/MuggleActivity.java
@@ -2,24 +2,26 @@ package top.est7.muggle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MuggleActivity extends AppCompatActivity {
 
-    private View mTextClock;
+    @BindView(R.id.text_clock)
+    TextClock mTextClock;
+    @BindView(R.id.activity_muggle)
+    LinearLayout mActivityMuggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muggle);
+        ButterKnife.bind(this);
 
-        mTextClock = findViewById(R.id.text_clock);
-
-
-        taozitest003
-        taozitest004;
-        taozi
-        桃子桃子123
-    }
+  }
 
 }
